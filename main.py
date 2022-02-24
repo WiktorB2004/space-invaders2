3bde342eeae485f33ef2d02309abc78f56b255f2
Wiktor <byrka.w@gmail.com>
2022-02-25 00:11:24 +0100

    implemented whole game logic

--- main.py
+++ main.py
@@ -178,28 +178,91 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (HEIGHT - level_label.get_width() - 10, 10))
         
+        for enemy in enemies:
+            enemy.draw(WIN)
+        
         player.draw(WIN)
         
+        if lost:
+            lost_label = lost_font.render('You Lost!!', 1, (255, 255, 255))
+            WIN.blit(lost_label, (WIDTH//2 - lost_label.get_width()//2, 350))
+            
         pygame.display.update()
     
     while run:
         clock.tick(FPS)
         redraw_window()
+        
+        if lives == 0 or player.health <= 0:
+            lost = True
+            lost_count += 1
+        
+        if lost:
+            if lost_count > FPS * 3:
+                run = False
+                break
+            else:
+                continue
+        
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, -100), random.choice(['red', 'blue', 'green']))
+                enemies.append(enemy)
+                
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player.shoot()
         keys = pygame.key.get_pressed()
-        
+
         if keys[pygame.K_a] and player.x - player_vel > 0: # Left
             player.x -= player_vel
         if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH: # Right
             player.x += player_vel
         if keys[pygame.K_w] and player.y - player_vel > 0: # Up
             player.y -= player_vel
-        if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: # Down
+        if keys[pygame.K_s] and player.y + player_vel + player.get_height() + 15 < HEIGHT: # Down
             player.y += player_vel
         
-        
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            enemy.move_lasers(laser_vel, player)
             
+            if random.randrange(0, 3*60) == 1:
+                enemy.shoot()
+            
+            if collide(enemy, player):
+                player.health -= 10
+                enemies.remove(enemy)
+            elif enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+                
+        player.move_lasers(-laser_vel, enemies)
+
+
+def main_menu():
+    title_font = pygame.font.SysFont('comicsans', 40)
+    run = True
+    while run:
+        WIN.blit(BG, (0, 0))
+        title_label = title_font.render('Press the mouse button to begin...', 1, (255, 255, 255))
+        WIN.blit(title_label, (WIDTH//2 - title_label.get_width()//2, 350))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+    pygame.quit()
+    
+    
 if __name__ == '__main__':
-    main()
+    main_menu()
+    
+
+
